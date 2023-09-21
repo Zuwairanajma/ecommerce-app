a23834a9f4889e6411cf3415a717ba6cc869b4b0
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-09-21 19:21:56 +0100

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -23,9 +23,19 @@ const rootElement = document.getElementById('root');
 
 // Use createRoot to render your application
 const root = ReactDOM.createRoot(rootElement);
+// const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  console.log('Element with ID "root" exists.');
+  // You can proceed with rendering your React application here
+} else {
+  console.error('Element with ID "root" does not exist.');
+  // Handle this situation, e.g., display an error message
+}
 
 root.render(
   <Provider store={store}>
     <App />
   </Provider>,
+  document.getElementById('root'),
 );
